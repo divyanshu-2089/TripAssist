9fc5cb6f7ab49f44d9feb4f2e6f61c722aff462a
divyanshu-2089 <127191492+divyanshu-2089@users.noreply.github.com>
2026-05-23 00:28:02 +0530

    Update connectDB.js

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://divyanshuk:Tripassist123@tripassist.xl7qcdg.mongodb.net/?appName=TripAssist');
+    await mongoose.connect('mongodb+srv://divyanshuk:XYZ@tripassist.xl7qcdg.mongodb.net/?appName=TripAssist');
     console.log("MongoDB Connected");
   } catch (error) {
     console.log("error to connect DB", error);
